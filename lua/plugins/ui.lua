dd2af79ef387ab72f43de6557a0acd98476ce062
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-16 15:39:47 +0200

    Remove bufferline and add tabline and windowbar
    I prefer using Telescope to navigate buffers.
    This shows the filename for the files at the top,
     and adds a prettier tab experience if tabs are used

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -27,11 +27,46 @@ return {
     opts = function(_, opts)
       local custom_auto = require("lualine.themes.auto")
       custom_auto.normal.c.bg = "None"
+      custom_auto.inactive.c.bg = "None"
       opts.options = {
         theme = custom_auto,
         section_separators = { left = "", right = "" },
         component_separators = "▎",
       }
+      opts.winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {
+          { "filetype", icon_only = true, separator = "" },
+          { "filename", file_status = true },
+        },
+      }
+      opts.inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          { "filename", file_status = true, separator = "" },
+          { "filetype", icon_only = true },
+        },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      }
+      opts.tabline = {
+        lualine_a = { "tabs" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "windows" },
+      }
     end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
   },
 }
