be0d19407992ca178627a4b1cef38718ccba8492
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-16 11:39:14 +0200

    Add borders around ui elements

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      window = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
+}
