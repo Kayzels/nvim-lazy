ff85b9327d0ab7c4cf2bcba2a8ace4aa7e1d0809
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-16 15:51:34 +0200

    Add autocommand to move help windows right
    Instead of opening a help window below by default, open on the right

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Move help window to the right when opened
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("help_window_right", {}),
+  pattern = { "*.txt" },
+  callback = function()
+    if vim.o.filetype == "help" then
+      vim.cmd.winc("L")
+    end
+  end,
+})
