34dbf7e781d20463cdd1fc6ae4ca986f37d1dd13
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-16 18:32:10 +0200

    Add vimtex completion

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,12 +1,14 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    dependencies = { "micangl/cmp-vimtex" },
     opts = function(_, opts)
       local cmp = require("cmp")
       opts.window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       }
+      table.insert(opts.sources, { name = "vimtex" })
     end,
   },
   {
