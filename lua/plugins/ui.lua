e5dd212330d1f26794dd59a19777988a2e648fb5
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-18 16:21:17 +0200

    Fix Neo-tree appearance

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -73,4 +73,27 @@ return {
     "akinsho/bufferline.nvim",
     enabled = false,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("BufEnter", {
+        -- make a group to be able to delete it later
+        group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+        callback = function()
+          local f = vim.fn.expand("%:p")
+          if vim.fn.isdirectory(f) ~= 0 then
+            vim.cmd("Neotree current dir=" .. f)
+            -- neo-tree is loaded now, delete the init autocmd
+            vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+          end
+        end,
+      })
+    end,
+    opts = {
+      popup_border_style = "rounded",
+      filesystem = {
+        hijack_netrw_behaviour = "open_current",
+      },
+    },
+  },
 }
