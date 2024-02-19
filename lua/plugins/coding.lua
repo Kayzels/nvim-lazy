d09acc8194ab7ebbe3c8ff70f1b88ef695e3c0a5
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-19 13:23:56 +0200

    Setup custom VSCode snippets

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,21 @@
 return {
   {
+    "L3MON4D3/LuaSnip",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = vim.fn.stdpath("config") .. "/lua/snippets/vscode/",
+      })
+    end,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load({
+          exclude = { "tex" }, -- ignore friendly snippets tex
+        })
+      end,
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     dependencies = { "micangl/cmp-vimtex" },
     opts = function(_, opts)
