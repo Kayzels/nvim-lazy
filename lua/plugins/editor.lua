1324988255653602c0803d53cce2cc5e3b445867
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-19 21:52:20 +0200

    Add Arrow plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -15,4 +15,20 @@ return {
       },
     },
   },
+  {
+    "otavioschwanck/arrow.nvim",
+    opts = function(_, opts)
+      opts.show_icons = true
+      opts.leader_key = "<leader>\\"
+      local width = 50
+      local col = math.ceil((vim.o.columns - width) / 2)
+      opts.window = {
+        width = width,
+        col = col,
+      }
+    end,
+    keys = {
+      { "<leader>\\", desc = "Arrow" },
+    },
+  },
 }
