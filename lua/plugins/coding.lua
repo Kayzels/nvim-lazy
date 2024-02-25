46bf6294bb3215cb8c640cb83e81e9e28db95d2a
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-25 15:37:10 +0200

    Change completion keybindings

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -26,17 +26,53 @@ return {
         })
       end,
     },
+    keys = {
+      { "<Tab>", mode = { "i", "s" }, false },
+      { "<S-Tab>", mode = { "i", "s" }, false },
+    },
   },
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "micangl/cmp-vimtex" },
+    dependencies = {
+      "micangl/cmp-vimtex",
+      "saadparwaiz1/cmp_luasnip",
+    },
     opts = function(_, opts)
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
       opts.window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       }
-      table.insert(opts.sources, { name = "vimtex" })
+      opts.sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "vimtex" },
+        { name = "luasnip" },
+        { name = "buffer" },
+      })
+      opts.mapping["<CR>"] = nil
+      opts.mapping["<S-CR>"] = cmp.mapping.confirm({ select = true })
+      opts.mapping["<C-CR>"] = cmp.mapping.confirm({ select = true })
+      opts.mapping["<S-Space>"] = cmp.mapping.abort()
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" })
       opts.formatting = {
         format = function(entry, item)
           local icons = require("lazyvim.config").icons.kinds
@@ -62,6 +98,25 @@ return {
       timeout = vim.o.timeoutlen,
       clear_empty_lines = false,
       keys = "<Esc>",
+    },
+  },
+  {
+    "abecodes/tabout.nvim",
+    lazy = false,
+    opts = {},
+    keys = {
+      {
+        "<A-x>",
+        "<Plug>(TaboutMulti)",
+        mode = "i",
+        desc = "Tabout Multi",
+      },
+      {
+        "<A-z>",
+        "<Plug>(TaboutBackMulti)",
+        mode = "i",
+        desc = "Tabout Back Multi",
+      },
     },
   },
 }
