5af9b00631048148e861805f2e116e3d382f83af
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-02-25 15:31:47 +0200

    Change Vimtex output directory

--- lua/plugins/tex.lua
+++ lua/plugins/tex.lua
@@ -1,3 +1,12 @@
+local function GetOutputDir()
+  local file_name = vim.fn.expand("%:t")
+  if file_name == "notes.tex" then
+    return "./out/"
+  else
+    return "../out/chapters/"
+  end
+end
+
 return {
   {
     "lervag/vimtex",
@@ -6,6 +15,7 @@ return {
         callback = 1,
         continuous = 0,
         executable = "latexmk",
+        out_dir = GetOutputDir,
         options = {
           "-verbose",
           "-file-line-error",
