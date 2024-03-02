6d5a8e1745669261fa07886fa9c5575488e930cc
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-03-02 10:47:40 +0200

    Add camel case motion for words

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -93,4 +93,26 @@ return {
     event = "InsertEnter",
     opts = {},
   },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    opts = {},
+    keys = {
+      {
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        mode = { "n", "o", "x" },
+      },
+    },
+  },
 }
