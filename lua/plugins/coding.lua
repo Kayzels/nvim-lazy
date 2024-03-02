dab306d6ff1b508f15a0c156433a07f4d2bb9399
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-03-02 11:51:19 +0200

    Add plugin for different text cases
    Useful for when wanting to set titlecase text,
    or switching cases.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -115,4 +115,25 @@ return {
       },
     },
   },
+  {
+    -- NB: Only using this fork until merged into "johmsalas/text-case.nvim"
+    "theutz/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup({})
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      "ga",
+      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+    },
+    cmd = {
+      "Subs",
+      "TextCaseOpenTelescope",
+      "TextCaseOpenTelescopeQuickChange",
+      "TextCaseOpenTelescopeLSPChange",
+      "TextCaseStartReplacingCommand",
+    },
+    lazy = true,
+  },
 }
