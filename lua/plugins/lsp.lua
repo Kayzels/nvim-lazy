ee4a6076a89d46cdd83034473968ab4ff8c67d17
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-07-08 16:38:12 +0200

    Update LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,12 +14,75 @@ end
 
 vim.api.nvim_create_user_command("ToggleLtex", toggle_ltex, {})
 vim.keymap.set("n", "<leader>ux", "<cmd>ToggleLtex<cr>", { desc = "Toggle Ltex" })
+
 return {
   {
     "neovim/nvim-lspconfig",
     optional = true,
     opts = {
+      inlay_hints = {
+        enabled = true,
+      },
+      codelens = {
+        enabled = false,
+      },
       servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              codeLens = {
+                enable = true,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+              hint = {
+                enable = true,
+                setType = true,
+                arrayIndex = "Disable",
+              },
+            },
+          },
+        },
+        pyright = {
+          enabled = false,
+        },
+        basedpyright = {
+          enabled = true,
+          settings = {
+            basedpyright = {
+              analysis = {
+                diagnosticSeverityOverrides = {
+                  reportMissingSuperCall = "none",
+                  reportPrivateUsage = "none",
+                  reportUnusedCallResult = "none",
+                  reportAny = "none",
+                  reportUninitializedInstanceVariable = "none",
+                  reportUnusedVariable = "none",
+                },
+              },
+            },
+          },
+        },
+        html = {
+          filetypes = {
+            "html",
+            "xhtml",
+          },
+          settings = {
+            html = {
+              format = {
+                wrapLineLength = 0,
+                indentInnerHtml = false,
+                maxPreserveNewLines = 2,
+                -- wrapLineWidth = 9999,
+              },
+            },
+          },
+        },
         texlab = {
           keys = {
             { "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
@@ -28,6 +91,9 @@ return {
             texlab = {
               diagnostics = {
                 ignoredPatterns = { "Underfull", "Overfull", "You have requested" },
+              },
+              experimental = {
+                labelReferenceCommands = { "nameref" },
               },
             },
           },
@@ -52,6 +118,21 @@ return {
             },
           },
         },
+        jsonls = {
+          on_new_config = function(new_config)
+            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+            local storeschemas = require("schemastore").json.schemas()
+            vim.list_extend(new_config.settings.json.schemas, storeschemas)
+          end,
+          settings = {
+            json = {
+              format = {
+                enable = true,
+              },
+              validate = { enable = true },
+            },
+          },
+        },
       },
       setup = {
         ltex = function()
@@ -69,8 +150,27 @@ return {
         end,
       },
     },
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<c-k>", false, mode = "i" }
+      -- keys[#keys + 1] = { "<c-i>", vim.lsp.buf.signature_help, mode = "i" }
+      keys[#keys + 1] = { "]]", false }
+      -- stylua: ignore
+      keys[#keys + 1] = { "]r", function() LazyVim.lsp.words.jump(vim.v.count1) end, has = "documentHighlight", desc = "Next Reference" }
+      keys[#keys + 1] = { "[[", false }
+      -- stylua: ignore
+      keys[#keys + 1] = { "[r", function() LazyVim.lsp.words.jump(-vim.v.count1) end, has = "documentHighlight", desc = "Prev Reference" }
+    end,
   },
   {
     "barreiroleo/ltex-extra.nvim",
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
+    },
   },
 }
