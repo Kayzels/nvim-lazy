779fcac3ce934bbd7ae608da92c4a4c0d1490dd0
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-07-08 13:53:59 +0200

    Set powershell and statuscol options
    Manually setting powershell instead of using LazyVim, due to ANSI
    sequences messing up output

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,29 @@ vim.opt.scrolloff = 10
 vim.diagnostic.config({
   float = { border = "rounded" },
 })
+
+vim.g.python3_host_prog = "C:\\Users\\Kyzan\\Tools\\nvim-venv\\Scripts\\python.exe"
+
+local function setPowershell()
+  vim.opt.shell = "powershell"
+  if vim.fn.executable("pwsh") then
+    vim.opt.shell = "pwsh"
+  end
+  vim.opt.shellcmdflag =
+    "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -NonInteractive -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;"
+  vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.opt.shellpipe = '2>&1 | %%{ "$_" } | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
+
+setPowershell()
+
+-- LazyVim.terminal.setup("pwsh")
+
+vim.opt.formatoptions = "jcroqlt"
+
+vim.g.lazyvim_statuscolumn = {
+  folds_open = false,
+  folds_githl = true,
+}
