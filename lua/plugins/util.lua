1e36ff3ad29f8618f07d6ac4085a579e4f08b2b8
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-08-11 13:54:09 +0200

    Update Telescope display

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -5,6 +5,16 @@ return {
   },
   {
     "telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "%.png", "%.jpg", "%.jpeg", "%.pdf" },
+        path_display = {
+          filename_first = {
+            reverse_directories = true,
+          },
+        },
+      },
+    },
     dependencies = {
       {
         "olimorris/persisted.nvim",
