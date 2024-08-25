402678b504d7b8a863d8f609bf557e8bd00995a5
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-08-25 11:57:25 +0200

    Add way to set Wezterm tab title

--- lua/plugins/wezterm.lua
+++ lua/plugins/wezterm.lua
@@ -1,10 +1,28 @@
 return {
   {
     "willothy/wezterm.nvim",
-    config = function(_, opts)
-      require("wezterm").setup(opts)
-
-      require("wezterm").set_tab_title("nvim")
-    end,
+    keys = {
+      {
+        "<leader>Wt",
+        function()
+          local tab_title = vim.fn.input("Enter new tab title: ")
+          if not tab_title then
+            return
+          else
+            require("wezterm").set_tab_title(tab_title)
+          end
+        end,
+        desc = "Set Wezterm Tab Title",
+      },
+    },
+    opts = {},
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>W", group = "Wezterm" },
+      },
+    },
   },
 }
