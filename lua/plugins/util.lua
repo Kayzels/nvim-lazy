eea0bfca7d257b46ead180e86b0849ba5d2ff6aa
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-09-30 13:32:33 +0200

    fix: Add extra ignore patterns to Telescope
    Telescope is listing some unwanted binary files that are generated when
    building a LaTeX file, so ignore those in the file list.

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -3,7 +3,17 @@ return {
     "telescope.nvim",
     opts = {
       defaults = {
-        file_ignore_patterns = { "%.png", "%.jpg", "%.jpeg", "%.pdf" },
+        file_ignore_patterns = {
+          "%.png",
+          "%.jpg",
+          "%.jpeg",
+          "%.pdf",
+          "%.pygtex",
+          "%.gz",
+          "%.fdb_latexmk",
+          "%.fls",
+          "%.aux",
+        },
         path_display = {
           filename_first = {
             reverse_directories = true,
