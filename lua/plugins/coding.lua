bbea62cf0ae46e200ff49cf7a046f52e492c806d
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-09-30 09:47:17 +0200

    feat: Use LazyVim markdown extra

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -369,15 +369,6 @@ return {
     },
   },
   {
-    "MeanderingProgrammer/markdown.nvim",
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("render-markdown").setup({})
-    end,
-    ft = { "md" },
-  },
-  {
     "nvim-cmp",
     dependencies = {
       "kawre/neotab.nvim",
