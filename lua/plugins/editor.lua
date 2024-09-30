f1bfbe49e9ee56e4adbf67a35c5a8015eb72b569
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-09-30 09:33:14 +0200

    refactor!: Remove arrow.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,21 +1,5 @@
 return {
   {
-    "otavioschwanck/arrow.nvim",
-    opts = function(_, opts)
-      opts.show_icons = true
-      opts.leader_key = "<leader>\\"
-      local width = 50
-      local col = math.ceil((vim.o.columns - width) / 2)
-      opts.window = {
-        width = width,
-        col = col,
-      }
-    end,
-    keys = {
-      { "<localleader>\\", desc = "Arrow" },
-    },
-  },
-  {
     "mikavilpas/yazi.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
