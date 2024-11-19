0b428aa53e61a741b5a64d6cbb44b1e7f75faafa
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-11-19 11:57:00 +0200

    feat(snacks_dashboard): Fix plugin integrations
    Remove lualine bars from showing on dashboard, as well as mini-indent

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -107,9 +107,9 @@ return {
         section_separators = { left = "", right = "" },
         component_separators = "▎",
         disabled_filetypes = {
-          tabline = { "neo-tree", "dashboard" },
-          winbar = { "neo-tree", "dashboard" },
-          statusline = { "dashboard" },
+          tabline = { "neo-tree", "dashboard", "snacks_dashboard" },
+          winbar = { "neo-tree", "dashboard", "snacks_dashboard" },
+          statusline = { "dashboard", "snacks_dashboard" },
         },
         always_show_tabline = false,
       }
@@ -333,6 +333,40 @@ return {
     end,
     config = function(_, opts)
       require("mini.hipatterns").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "LazyFile",
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "snacks_dashboard",
+          "Trouble",
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "mason",
+          "neo-tree",
+          "notify",
+          "snacks_notif",
+          "snacks_terminal",
+          "snacks_win",
+          "toggleterm",
+          "trouble",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
     end,
   },
 }
