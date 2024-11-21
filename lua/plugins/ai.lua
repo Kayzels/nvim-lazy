c91c3e14b1bb14db678c8925acd87f8f4af7de0c
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-11-21 16:25:58 +0200

    feat(ai): Change from Codeium to NeoCodeium

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,7 +1,51 @@
 return {
   {
-    "Kayzels/codeium.nvim",
-    branch = "toggle",
-    dev = false,
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    config = function()
+      local neocodeium = require("neocodeium")
+      neocodeium.setup({
+        enabled = false,
+        manual = true,
+        silent = true,
+        filetypes = {
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          ["."] = false,
+          TelescopePrompt = false,
+          snacks_dashboard = false,
+          ["neo-tree"] = false,
+          ["dap-repl"] = false,
+          snacks_terminal = false,
+        },
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NeoCodeiumCompletionDisplayed",
+        callback = function()
+          require("cmp").abort()
+        end,
+      })
+
+      vim.keymap.set("i", "<A-f>", function()
+        neocodeium.accept()
+      end)
+      vim.keymap.set("i", "<A-w>", function()
+        neocodeium.accept_word()
+      end)
+      vim.keymap.set("i", "<A-a>", function()
+        neocodeium.accept_line()
+      end)
+      vim.keymap.set("i", "<A-e>", function()
+        neocodeium.cycle_or_complete()
+      end)
+      vim.keymap.set("i", "<A-r>", function()
+        neocodeium.cycle_or_complete(-1)
+      end)
+      vim.keymap.set("i", "<A-c>", function()
+        neocodeium.clear()
+      end)
+    end,
   },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -33,6 +33,36 @@ local lsp = {
   cond = conditions.hide_in_width,
 }
 
+local neocodium_icons = {
+  status = {
+    [0] = "󰚩 ", -- enabled
+    [1] = "󱚧 ", -- disabled globally
+    [2] = "󱙻 ", -- disabled for buffer
+    [3] = "󱙺 ", -- disabled for buffer filetype
+    [4] = "󱙺 ", -- disabled for buffer with enabled function
+    [5] = "󱚠 ", -- disabled for buffer encoding
+  },
+  server_status = {
+    [0] = "󰣺 ", -- server connected
+    [1] = "󰣻", -- server connecting
+    [2] = "󰣽", -- server disconnected
+  },
+}
+
+local neocodeium_status = {
+  ---@return string
+  function()
+    local status, server_status = require("neocodeium").get_status()
+    if server_status ~= 0 then
+      return neocodium_icons.server_status[server_status]
+    end
+    return neocodium_icons.status[status]
+  end,
+  color = function()
+    return LazyVim.ui.fg("Special")
+  end,
+}
+
 return {
   {
     "akinsho/bufferline.nvim",
@@ -184,7 +214,8 @@ return {
             end
           end,
         },
-        LazyVim.lualine.cmp_source("codeium"),
+        neocodeium_status,
+        -- LazyVim.lualine.cmp_source("codeium"),
         lsp,
         { "filetype", padding = { left = 1, right = 1 }, icon = { align = "right" }, cond = conditions.hide_in_width },
       }
