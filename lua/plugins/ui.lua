e68c1ea71dc0acd9068e3d0760b344085c7dabae
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-11-26 12:49:23 +0200

    feat(noice,lsp): Switch to pull request branch
    Fixes complaint from LSP about missing an offset_encoding and using a
    default one.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -94,7 +94,10 @@ return {
     },
   },
   {
-    "folke/noice.nvim",
+    -- NOTE: To fix nightly LSP offset encoding warning, until merged
+    "tris203/noice.nvim",
+    branch = "mixed_encodings",
+    -- "folke/noice.nvim",
     config = function(_, opts)
       -- NOTE: Calling explicitly rather than using LazyVim way,
       -- because LazyVim config clears messages
