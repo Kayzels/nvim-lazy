c6e2436922538a9374b1b0f9dbdacede4790bf5e
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-11-26 12:52:57 +0200

    feat(scroll): Remove scrollbars

--- lua/plugins/scroll.lua
+++ lua/plugins/scroll.lua
@@ -15,39 +15,4 @@ return {
       legacy_computing_symbols_support = false,
     },
   },
-  {
-    "lewis6991/satellite.nvim",
-    opts = {
-      winblend = 0,
-      handlers = {
-        cursor = {
-          enable = true,
-          symbols = { "•" },
-        },
-        gitsigns = {
-          enable = true,
-          signs = {
-            add = "▎",
-            change = "▎",
-            delete = "",
-          },
-        },
-      },
-    },
-  },
-  -- {
-  --   "dstein64/nvim-scrollview",
-  --   event = "LazyFile",
-  --   opts = {
-  --     -- always_show = true,
-  --     current_only = true,
-  --     excluded_filetypes = { "snacks_dashboard", "TelescopePrompt", "Noice", "neo-tree" },
-  --   },
-  -- },
-  -- {
-  --   "dstein64/nvim-scrollview",
-  --   opts = function(_, opts)
-  --     require("scrollview.contrib.gitsigns").setup()
-  --   end,
-  -- },
 }
