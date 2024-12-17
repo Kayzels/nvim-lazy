092ed0de4712168f6f4867da97b24cdf27849042
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2024-12-17 14:37:03 +0200

    feat(auto-dark): Change update interval
    Was set wrong for WSL, which led to Neovim hanging on exit.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -176,6 +176,7 @@ return {
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
+      update_interval = 1000,
       set_dark_mode = function()
         require("functions.theme").setMode("dark")
       end,
