bfd66011944d615f4a2d500634146f30256419c0
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-05-31 17:08:35 +0200

    feat(neovide): Add base Neovide config

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.print("1")
 if vim.g.vscode then
   require("vscode_custom")
+end
+if vim.g.neovide then
+  require("neovide")
 end
---
+++ lua/neovide/init.lua
@@ -0,0 +1,11 @@
+vim.o.guifont = "Maple Mono NF:h10:#e-subpixelantialias:#h-none"
+vim.g.transparency = 0
+vim.g.neovide_padding_top = 10
+vim.g.neovide_padding_bottom = 10
+vim.g.neovide_padding_left = 10
+vim.g.neovide_padding_right = 10
+
+-- TODO:
+-- Neovide is nice, but is there any way to set the environment before opening it?
+-- For example, to activate the python venv before opening?
+-- And makes opening extra terminals messy, I don't like that being inside Neovim.
